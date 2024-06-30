833a2c24999c067615a0d53f85cd4cba403ed144
evertonse <evertonse.junior@gmail.com>
2024-06-30 20:40:45 -0300

    Sun Jun 30 08:40:45 PM -03 2024

--- lua/personal/highlight.lua
+++ lua/personal/highlight.lua
@@ -436,7 +436,7 @@ local M = {
 
     IblIndent = { fg = c.text.LightDim },
     IblWhitespace = { fg = c.text.LightDim },
-    IblScope = { fg = c.text.Bright },
+    IblScope = { fg = c.text.Light },
     ['@ibl.scope.underline.1'] = { fg = c.text.Normal },
     ['@ibl.scope.underline'] = { link = '@ibl.scope.underline.1' },
     IblScopeChar = { link = 'IblScope' },
