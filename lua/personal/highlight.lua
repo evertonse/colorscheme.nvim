ee88183c4807c80cd8c2b149f0ebc6e977345997
evertonse <evertonse.junior@gmail.com>
2024-07-02 19:54:25 -0300

    Tue Jul  2 07:54:25 PM -03 2024

--- lua/personal/highlight.lua
+++ lua/personal/highlight.lua
@@ -17,7 +17,7 @@ local M = {
         fg = c.editor.CursorDark, --[[ bg = c.editor.CursorLight ]]
     },
     -- Set the cursorline background color
-    CursorLine = { fg = c.None, bg = c.None },
+    CursorLine = { fg = c.None, bg = c.Debug },
 
     CursorColumn = { fg = 'NONE', bg = c.editor.CursorDarkDark },
     Directory = {
